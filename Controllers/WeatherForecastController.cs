462cdea3a2ad7ced930e039f47bb1d6e7920577b
kusaaal123 <22kusal@gmail.com>
2026-08-19 08:59:51 +0545

    Fix log message punctuation in WeatherForecastController

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -21,7 +21,7 @@ namespace Sample_Dotnet_Api_Project.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            _logger.LogInformation("Getting weather forecast");
+            _logger.LogInformation("Getting weather forecast.");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
